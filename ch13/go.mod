cd6fa5db6f38d659bf96389cc8a2737f02c299c5
TestAquatic <86832451+testaquatic@users.noreply.github.com>
2025-04-07 01:45:25 +0900

    중간 저장

--- ch13/go.mod
+++ ch13/go.mod
@@ -6,9 +6,7 @@ require (
 	github.com/fsnotify/fsnotify v1.8.0
 	go.uber.org/multierr v1.11.0
 	go.uber.org/zap v1.27.0
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 )
 
-require (
-	golang.org/x/sys v0.13.0 // indirect
-	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
-)
+require golang.org/x/sys v0.13.0 // indirect
--- ch13/instrumentation/main.go
+++ ch13/instrumentation/main.go
@@ -1,9 +1,17 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"net/http"
+
+	"github.com/testaquatic/NetworkProgrammingWithGo/ch13/instrumentation/metrics"
+)
 
 var (
 	metricsAddr = flag.String("metrics", "127.0.0.1:8081", "metrics listen address")
 	webAddr = flag.String("web", "127.0.0.1:8082", "web listen address")
 )
 
+func helloHandler(w http.ResponseWriter, _ *http.Request) {
+	metrics.Requests
+}
